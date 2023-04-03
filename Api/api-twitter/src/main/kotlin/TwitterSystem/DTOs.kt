306ee8be0d1d0d70d8c792a2fa8eae6a5dbe4696
Ornella Mendez <hope.peque@gmail.com>
2023-04-02 22:26:52 -0300

    "arreglando cagadas"

--- Api/api-twitter/src/main/kotlin/TwitterSystem/DTOs.kt
+++ Api/api-twitter/src/main/kotlin/TwitterSystem/DTOs.kt
@@ -1,12 +1,12 @@
-package org.unq
+package TwitterSystem
 
 class UserLoginDTO(val username: String, val password: String)
 class DraftUserDTO(val usarme: String, val email: String, val password: String, val image: String, val backgroundImage: String)
 class SimpleUserDTO(val id: String, val username: String)
-class UserDTO(val id: String, val username: String, val email: String, val image: String, val backgroundImage: String,  var followers: List<SimpleUserDTO>, var following: List<SimpleUserDTO>, var tweets: List<SimpleTweetDTO>)
+class UserDTO(val id: String, val username: String, val email: String, val image: String, val backgroundImage: String, var followers: List<SimpleUserDTO>, var following: List<SimpleUserDTO>, var tweets: List<SimpleTweetDTO>)
 
 class TwitterTypeDTO(val tweet: SimpleTweetDTO, val image: String?)
-class SimpleTweetDTO(val id: String, val type: TwitterTypeDTO,  var user: SimpleUserDTO, val content: String, val date: String, val repliesAmount: Integer, val reTweetAmount: Integer, val likes: List<SimpleUserDTO>)
+class SimpleTweetDTO(val id: String, val type: TwitterTypeDTO, var user: SimpleUserDTO, val content: String, val date: String, val repliesAmount: Integer, val reTweetAmount: Integer, val likes: List<SimpleUserDTO>)
 class TweetDTO(val id: String, val type: TwitterTypeDTO, var user: SimpleUserDTO, val content: String, val date: String, val replies: List<SimpleTweetDTO>, val reTweet: List<SimpleTweetDTO>, val likes: List<SimpleUserDTO>)
 
 class TweetResulDTO(val result: List<SimpleTweetDTO>)
--- Api/api-twitter/src/main/kotlin/TwitterSystem/IdGenerator.kt
+++
@@ -1,9 +0,0 @@
-package org.unq
-
-class IdGenerator {
-    private var currentUserId = 0
-    private var currentTweetId = 0
-
-    fun nextUserId(): String = "u_${++currentUserId}"
-    fun nextTweetId(): String = "t_${++currentTweetId}"
-}
