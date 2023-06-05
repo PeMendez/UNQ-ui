eba6c7816bb56e5caecd8fcc27230bf7999f8842
Ornella Mendez <hope.peque@gmail.com>
2023-06-05 15:14:41 -0300

    isLike en SimpleTweetDTO

--- Api/src/main/kotlin/org/DTOs.kt
+++ Api/src/main/kotlin/org/DTOs.kt
@@ -5,21 +5,21 @@ import org.unq.*
 class UserLoginDTO(val username: String, val password: String)
 class DraftUserDTO(val username: String, val email: String, val password: String, val image: String, val backgroundImage: String)
 class SimpleUserDTO(val id: String, val username: String, val image: String)
-class UserDTO(val id: String, val username: String, val email: String, val image: String, val backgroundImage: String,  var followers: List<SimpleUserDTO>, var following: List<SimpleUserDTO>, var tweets: List<SimpleTweetDTO>){
-    constructor(user: User, tweets: List<Tweet>) : this(user.id, user.username, user.email, user.image, user.backgroundImage, listOf<SimpleUserDTO>(), listOf<SimpleUserDTO>(), listOf<SimpleTweetDTO>()) {
+class UserDTO(val id: String, isLike: Boolean, val username: String, val email: String, val image: String, val backgroundImage: String,  var followers: List<SimpleUserDTO>, var following: List<SimpleUserDTO>, var tweets: List<SimpleTweetDTO>){
+    constructor(user: User, tweets: List<Tweet>, isLike: Boolean) : this(user.id, isLike, user.username, user.email, user.image, user.backgroundImage, listOf<SimpleUserDTO>(), listOf<SimpleUserDTO>(), listOf<SimpleTweetDTO>()) {
         this.followers = user.followers.map { f -> SimpleUserDTO(f.id, f.username, f.image) }
         this.following = user.following.map { f -> SimpleUserDTO(f.id, f.username, f.image) }
-        this.tweets = tweets.map { t ->  SimpleTweetDTO(t)}
+        this.tweets = tweets.map { t ->  SimpleTweetDTO(t, isLike)}
 
     }}
 
-class TwitterTypeDTO(tweetType: TweetType) {
+class TwitterTypeDTO(tweetType: TweetType, isLiked: Boolean) {
     val image = tweetType.image
-    val tweet = if (tweetType.tweet == null) null else SimpleTweetDTO(tweetType.tweet!!)
+    val tweet = if (tweetType.tweet == null) null else SimpleTweetDTO(tweetType.tweet!!, isLiked)
 
 }
-class SimpleTweetDTO(val id: String, val type: TwitterTypeDTO, var user: SimpleUserDTO, val content: String, val date: String, val repliesAmount: Int, val reTweetAmount: Int, var likes: List<SimpleUserDTO>){
-    constructor(tweet: Tweet): this(tweet.id, TwitterTypeDTO(tweet.type), SimpleUserDTO(tweet.user.id, tweet.user.username, tweet.user.image), tweet.content, tweet.date.toString(), tweet.replies.size, tweet.reTweets.size, listOf<SimpleUserDTO>()){
+class SimpleTweetDTO(val id: String, isLiked: Boolean, val type: TwitterTypeDTO, var user: SimpleUserDTO, val content: String, val date: String, val repliesAmount: Int, val reTweetAmount: Int, var likes: List<SimpleUserDTO>){
+    constructor(tweet: Tweet, isLiked: Boolean): this(tweet.id, isLiked, TwitterTypeDTO(tweet.type, isLiked), SimpleUserDTO(tweet.user.id, tweet.user.username, tweet.user.image), tweet.content, tweet.date.toString(), tweet.replies.size, tweet.reTweets.size, listOf<SimpleUserDTO>()){
         this.likes = tweet.likes.map { l -> SimpleUserDTO(l.id, l.username, l.image) }
     }
 }
@@ -27,7 +27,7 @@ class TweetDTO(val id: String, val isLiked: Boolean, val type: TwitterTypeDTO, v
     constructor(tweet: Tweet, isLike: Boolean): this(tweet.id,
                                     isLike,
                                     TwitterTypeDTO(
-                                        tweet.type),
+                                        tweet.type, isLike),
                                     SimpleUserDTO(
                                         tweet.user.id,
                                         tweet.user.username,
@@ -37,8 +37,8 @@ class TweetDTO(val id: String, val isLiked: Boolean, val type: TwitterTypeDTO, v
                                     listOf<SimpleTweetDTO>(),
                                     listOf<SimpleTweetDTO>(),
                                     listOf<SimpleUserDTO>()){
-        this.reTweet = tweet.reTweets.map { r -> SimpleTweetDTO(r) }
-        this.replies = tweet.replies.map { r -> SimpleTweetDTO(r) }
+        this.reTweet = tweet.reTweets.map { r -> SimpleTweetDTO(r, isLike) }
+        this.replies = tweet.replies.map { r -> SimpleTweetDTO(r, isLike) }
         this.likes = tweet.likes.map { l -> SimpleUserDTO(l.id, l.username, l.image)}
 }}
 class TweetResulDTO(val result: List<SimpleTweetDTO>)
--- Api/src/main/kotlin/org/TweetController.kt
+++ Api/src/main/kotlin/org/TweetController.kt
@@ -15,13 +15,13 @@ class TweetController(private val twitterSystem: TwitterSystem) {
         if (query.isBlank()){
             throw BadRequestResponse("Invalid query")
         }
-        val searchResult = tweetToSimpleTweet(twitterSystem.search(query))
+        val searchResult = tweetToSimpleTweet(twitterSystem.search(query), ctx)
         ctx.json(TweetResulDTO(searchResult))
     }
 
     fun getTrendingTopics(ctx: Context){
 
-        val tweetResul = tweetToSimpleTweet(twitterSystem.getTrendingTopics())
+        val tweetResul = tweetToSimpleTweet(twitterSystem.getTrendingTopics(), ctx)
 
         ctx.json(TweetResulDTO(tweetResul))
     }
@@ -108,9 +108,9 @@ class TweetController(private val twitterSystem: TwitterSystem) {
 
         return DraftReplyTweet(user.id, originTweet.id, tweetDTO.content, tweetDTO.image, LocalDateTime.now())
     }
-    private fun tweetToSimpleTweet(list : List<Tweet>) : List<SimpleTweetDTO> {
+    private fun tweetToSimpleTweet(list : List<Tweet>, ctx: Context) : List<SimpleTweetDTO> {
 
-        return list.map { t -> SimpleTweetDTO(t) }
+        return list.map { t -> SimpleTweetDTO(t, isLiked(ctx, t)) }
     }
 
 }
