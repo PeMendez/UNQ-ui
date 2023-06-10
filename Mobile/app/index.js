676d7fc0cd31a99d07da7d61f371ff6eb9f39c60
Ornella Mendez <hope.peque@gmail.com>
2023-06-10 19:56:56 -0300

    andand los links parcialmente

--- Mobile/app/index.js
+++ Mobile/app/index.js
@@ -6,7 +6,8 @@ export default function Page() {
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Home</Text>
-        <Text style={styles.subtitle}>Esto es una mierda..</Text>
+        <Link href="/search">Search</Link>
+        <Text style={styles.subtitle}>Esto es una mierda...</Text>
         <Link href="/profile">Go to profile</Link>
       </View>
     </View>
--- Mobile/app/profile.js
+++ Mobile/app/profile.js
@@ -1,14 +1,36 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { Link } from "expo-router";
 
 export default function Profile() {
     return (
         <View style={styles.container}>
           <View style={styles.main}>
-            <Text style={styles.title}>Home</Text>
-            <Text style={styles.subtitle}>This is the first page of your app.</Text>
-            <Link href="/profile">Go to profile</Link>
+            <Text style={styles.title}>Profile</Text>
+            <Link href="/index">Go index</Link>
           </View>
         </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
 
